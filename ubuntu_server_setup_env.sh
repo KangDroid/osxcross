20c36c916f90b8dd0954ba3095c51bbbdd51f0d4
KangDroid <hyunwoo286@gmail.com>
2020-08-11 15:15:29 +0900

    Add Ubuntu Server environment setup script

---
+++ ubuntu_server_setup_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Only for Ubuntu 20.04.1 Server[Desktop should work, but anyway, its not tested]
+sudo apt-get install build-essential clang gcc g++ zlib1g-dev libmpc-dev libmpfr-dev libgmp-dev cmake git libxml2-dev libssl-dev zlib1g-dev
